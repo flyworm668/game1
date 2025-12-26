ee9cb698cba642d59793ab68f9db3a48a7f2516f
梁高嘉 <16380010+liang-gaojia@user.noreply.gitee.com>
2025-12-26 09:42:09 +0800

    更新2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 注意：如果你要部署到 GitHub Pages 的 /tree/ 子路径，请取消下面这行的注释
-  // base: '/tree/', 
+  // base: '/game1/', 
 });
